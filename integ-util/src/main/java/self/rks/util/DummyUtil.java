d7c0f27b9a9ac2ee36b3b3004c43a24a842254e6
Ravi Sinha <ravi.k.sinha.dev@gmail.com>
2015-08-02 09:17:00 +0530

    Created a dummy util class to be tested as a project dependency in webapp project.

---
+++ integ-util/src/main/java/self/rks/util/DummyUtil.java
@@ -0,0 +1,13 @@
+package self.rks.util;
+
+public class DummyUtil {
+
+    /**
+     * A dummy method to play around
+     * @return a dummy value.
+     */
+    public static String getDummyName() {
+        return "DummyName";
+    }
+
+}
--- integ-web-app/src/main/java/self/rks/servlet/ServletOne.java
+++ integ-web-app/src/main/java/self/rks/servlet/ServletOne.java
@@ -1,5 +1,7 @@
 package self.rks.servlet;
 
+import self.rks.util.DummyUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,10 +14,15 @@ public class ServletOne extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        DummyUtil.getDummyName();
+
         PrintWriter out = resp.getWriter();
         out.println("<html>");
         out.println("<body>");
         out.println("<h1>Hello</h1>");
+        out.println("<h2>");
+        out.println("The value from DummyUtil.getDummyName() is " + DummyUtil.getDummyName());
+        out.println("<h2>");
         out.println("</body>");
         out.println("</html>");
 
