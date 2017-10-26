9e86137f5e3134de643012c1815755f9c96842df
Ravi K Sinha <ravi.k.sinha.dev@gmail.com>
2017-10-26 15:25:50 +0530

    Implemented Insertion Sort and its tests

---
+++ integ-java-experiments/src/main/java/ds/InsertionSort.java
@@ -0,0 +1,35 @@
+package ds;
+
+/**
+ * Implements the classic insertion sort routine
+ * Algo is as follows
+ * <li>Start from the an index, and mark it as index with lowest value</li>
+ * <li>Compare with elements to the left of it till we see smaller values or until we reach the end of the array</li>
+ * <li>If another index has lower value, mark it as having lowest value</li>
+ * <li>At the end of the iteration, swap the current lowest value index with index marked in the beginning</li>
+ *
+ * In essence we are inserting lowest value successively in increasing order indexes
+ */
+public class InsertionSort {
+
+    public int[] sortInts(int[] unsortedArray) {
+
+        int currentElem = -1;
+        int position = -1;
+
+        for (int i = 1; i < unsortedArray.length; i++) {
+            currentElem = unsortedArray[i]; // Init with current index
+            position = i; // Init with current index
+
+            while(position > 0 && unsortedArray[position - 1] > currentElem) {
+                unsortedArray[position] = unsortedArray[position - 1];
+                position = position - 1;
+            }
+
+            unsortedArray[position] = currentElem;
+        }
+
+        return unsortedArray;
+    }
+
+}
---
+++ integ-java-experiments/src/test/java/ds/InsertionSortTests.java
@@ -0,0 +1,40 @@
+package ds;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class InsertionSortTests {
+
+    private InsertionSort is;
+
+    @Before
+    public void setup() {
+        is = new InsertionSort();
+    }
+
+    @Test
+    public void testInsertionSort_1() {
+        int[] inputArray = new int[] {4, 7, 1, 3, 2};
+        int[] expectedArray = new int[] {1, 2, 3, 4, 7};
+        int[] result = is.sortInts(inputArray);
+
+        System.out.println(Arrays.toString(result));
+
+        Assert.assertArrayEquals(expectedArray, result);
+    }
+
+    @Test
+    public void testInsertionSort_2() {
+        int[] inputArray = new int[] {1, 2, 3, 4, 7};
+        int[] expectedArray = new int[] {1, 2, 3, 4, 7};
+        int[] result = is.sortInts(inputArray);
+
+        System.out.println(Arrays.toString(result));
+
+        Assert.assertArrayEquals(expectedArray, result);
+    }
+
+}
